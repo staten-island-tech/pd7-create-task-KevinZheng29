592deb40e1dfc8591b1ec2defa71dea50e87df7a
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-02-09 14:01:24 -0500

    1

--- csp/javascript/main.js
+++ csp/javascript/main.js
@@ -9,18 +9,13 @@ const DOM = {
   historyresult: document.getElementById("historyresult"),
 };
 
-const history = [
-  {
-    win: 0,
-  },
-  {
-    lost: 0,
-  },
-];
+const history = [];
 
 let card1number;
 let card2number;
 let card3number;
+
+let jokernumber = Math.floor(Math.random() * 3) + 1;
 
 let a = Math.floor(Math.random() * 3) + 1;
 let b = Math.floor(Math.random() * 3) + 1;
@@ -44,25 +39,43 @@ function generatenumber(number1, number2, number3) {
 generatenumber(a, b, c);
 
 DOM.card1.addEventListener("click", function () {
-  if (card1number === 1) {
+  if (card1number === jokernumber) {
     DOM.page.innerHTML = "You got joker. You lose!";
+    history.push("Lose");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   } else {
     DOM.page.innerHTML = "You didn't get the joker. You win!";
+    history.push("Win");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   }
 });
 
+console.log(history);
+
 DOM.card2.addEventListener("click", function () {
-  if (card2number === 1) {
+  if (card2number === jokernumber) {
     DOM.page.innerHTML = "You got joker. You lose!";
+    history.push("Lose");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   } else {
     DOM.page.innerHTML = "You didn't get the joker. You win!";
+    history.push("Win");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   }
 });
 
 DOM.card3.addEventListener("click", function () {
-  if (card3number === 1) {
+  if (card3number === jokernumber) {
     DOM.page.innerHTML = "You got joker. You lose!";
+    history.push("Lose");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   } else {
     DOM.page.innerHTML = "You didn't get the joker. You win!";
+    history.push("Win");
+    jokernumber = Math.floor(Math.random() * 3) + 1;
   }
+});
+
+historybtn.addEventListener("click", function () {
+  historyresult.innerHTML = history;
 });
